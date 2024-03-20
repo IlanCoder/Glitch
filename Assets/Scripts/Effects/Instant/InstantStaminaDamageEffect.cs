e76e6b7434dcf5fc4733195fa26a1eb33527a051
Ilan González Carigi <ilancoder00@gmail.com>
2024-03-19 20:28:26 -0600

    Update InstantStaminaDamageEffect.cs

--- Assets/Scripts/Effects/Instant/InstantStaminaDamageEffect.cs
+++ Assets/Scripts/Effects/Instant/InstantStaminaDamageEffect.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using Characters;
 
 namespace Effects.Instant {
+    [CreateAssetMenu(fileName = "InstantStaminaDamage",menuName = "Effects/Instant/Stamina Damage")]
     public class InstantStaminaDamageEffect : InstantCharacterEffect {
         public float staminaDamage;
         
