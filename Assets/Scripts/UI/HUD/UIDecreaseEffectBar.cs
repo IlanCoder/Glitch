256ea3bb77ae4d86cb0f8409ba39c6cbd5fec7d1
Ilan González Carigi <ilancoder00@gmail.com>
2024-07-31 17:10:27 -0600

    Color decrease bars

---
+++ Assets/Scripts/UI/HUD/UIDecreaseEffectBar.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.HUD {
+    public class UIDecreaseEffectBar : MonoBehaviour {
+
+        RectTransform _transform;
+        Slider _slider;
+        [SerializeField] Slider parentSlider;
+
+        float decreasePerSec;
+        float enabledTime;
+
+        [Header("Bar Options")]
+        [SerializeField, Tooltip("% per second")] float decreaseSpeed;
+        [SerializeField] float decreaseDelay;
+
+        void Awake() {
+            _transform = GetComponent<RectTransform>();
+            _slider = GetComponent<Slider>();
+            gameObject.SetActive(false);
+        }
+
+        void LateUpdate() {
+            if (Time.time - enabledTime < decreaseDelay) return;
+            if (_slider.value <= parentSlider.value) {
+                gameObject.SetActive(false);
+                return;
+            }
+            float val = _slider.value - decreasePerSec * Time.deltaTime;
+            _slider.value = val;
+        }
+
+        public void EnableDecreaseSlider(float initVal) {
+            _slider.value = initVal;
+            enabledTime = Time.time;
+            gameObject.SetActive(true);
+        }
+
+        public void SetMaxVal(float maxVal, float widthScaleMultiplier, bool scale = true) {
+            _slider.maxValue = maxVal;
+            decreasePerSec = maxVal / 100 * decreaseSpeed;
+            if (!scale) return;
+            _transform.sizeDelta = new Vector2(maxVal * widthScaleMultiplier, _transform.sizeDelta.y);
+        }
+    }
+}
--- Assets/Scripts/UI/HUD/UIStatBar.cs
+++ Assets/Scripts/UI/HUD/UIStatBar.cs
@@ -1,5 +1,7 @@
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 namespace UI.HUD {
     public class UIStatBar : MonoBehaviour {
@@ -9,17 +11,28 @@ namespace UI.HUD {
         [Header("Bar Options")]
         [SerializeField] bool scaleBarLengthWithStats = true;
         [SerializeField] float widthScaleMultiplier = 1;
+
+        [Header("Decrease Effect Bar")]
+        [SerializeField] UIDecreaseEffectBar decreaseEffect;
+
         protected void Awake() {
             _slider = GetComponent<Slider>();
             _transform = GetComponent<RectTransform>();
         }
 
         public void SetStat(float val) {
+            float diff = _slider.value - val;
+            float oldVal = _slider.value;
             _slider.value = val;
+            if (diff <= _slider.maxValue / 100) return;
+            if (decreaseEffect.gameObject.activeSelf) return;
+            decreaseEffect.EnableDecreaseSlider(oldVal);
         }
 
         public void SetMaxStat(float maxVal) {
+            if (maxVal < _slider.value) _slider.value = maxVal;
             _slider.maxValue = maxVal;
+            if (decreaseEffect) decreaseEffect.SetMaxVal(maxVal, widthScaleMultiplier, scaleBarLengthWithStats);
             if (!scaleBarLengthWithStats) return;
             _transform.sizeDelta = new Vector2(maxVal * widthScaleMultiplier, _transform.sizeDelta.y);
             gameObject.SetActive(false);
