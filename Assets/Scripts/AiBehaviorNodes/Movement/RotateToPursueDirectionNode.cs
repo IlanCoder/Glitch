eab4324b22630dd72c48ffcd29a79828808fde13
Ilan González Carigi <ilancoder00@gmail.com>
2024-08-01 19:58:11 -0600

    Attacks

--- Assets/Scripts/AiBehaviorNodes/Movement/RotateToPursueDirectionNode.cs
+++ Assets/Scripts/AiBehaviorNodes/Movement/RotateToPursueDirectionNode.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 namespace AiBehaviorNodes.Movement {
     public class RotateToPursueDirectionNode : LeafNode {
-        Vector3 targetDirection;
+        Vector3 _targetDirection;
         
         protected override void InitializeNode() {
             NpcAgent.animController.ApplyRootMotion(true);
-            targetDirection = NpcAgent.movementController.GetNavAgentRotation();
+            _targetDirection = NpcAgent.movementController.GetNavAgentRotation();
         }
 
         protected override NodeStatus Tick() {
             if (!TreeBlackboard.targetCharacter) return NodeStatus.Failed;
-            if (NpcAgent.movementController.IsDirectionStraightAhead(targetDirection))
+            if (NpcAgent.movementController.IsDirectionStraightAhead(_targetDirection))
                 return NodeStatus.Succeeded;
-            NpcAgent.movementController.RotateTowardsDirection(targetDirection);
+            NpcAgent.movementController.RotateTowardsDirection(_targetDirection);
             return NodeStatus.Running;
         }
 
