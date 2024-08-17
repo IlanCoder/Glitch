c8d56028e4c04133e57bf1773acae6bbf0d4ca0d
Ilan González Carigi <ilancoder00@gmail.com>
2024-08-16 18:39:38 -0600

    Boss name

--- Assets/Scripts/UI/CombatUI/EnemiesStatBars/BossStatBarsController.cs
+++ Assets/Scripts/UI/CombatUI/EnemiesStatBars/BossStatBarsController.cs
@@ -1,7 +1,21 @@
-﻿namespace UI.CombatUI.EnemiesStatBars {
+﻿using Characters;
+using TMPro;
+using UnityEngine;
+
+namespace UI.CombatUI.EnemiesStatBars {
     public class BossStatBarsController : NpcStatBarsController {
+
+        [Header("Texts")]
+        [SerializeField] TextMeshProUGUI nameText;
+        [SerializeField] TextMeshProUGUI damageText;
+        
         protected void Start() {
             gameObject.SetActive(false);
+        }
+
+        override public void TieNewCharacter(CharacterManager newTarget, bool inCombat = false, int damageReceived = 0) {
+            base.TieNewCharacter(newTarget, inCombat, damageReceived);
+            nameText.text = newTarget.name;
         }
     }
 }
